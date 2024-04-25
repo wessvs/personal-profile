3eaaac5864690e084a63b5df2e58092f95ea2f36
Wessley Silva <wessley.silva@outlook.com.br>
2024-04-25 19:50:15 -0300

    adding state to follow button

--- src/components/Profile/ProfileSection/index.jsx
+++ src/components/Profile/ProfileSection/index.jsx
@@ -2,7 +2,9 @@ import styles from './styles.module.css'
 
 export default function ProfileSection(props) {
     return (
-        <div className={styles.wrapper}>
+        <div
+        {...props} 
+        className={`${styles.wrapper} ${props.className}`}>
             {props.children}
         </div>
     )
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,27 +2,42 @@ import Title from './Title'
 import styles from './styles.module.css'
 import ProfileSection from './ProfileSection'
 import LinkButton from '../LinkButton'
+import { useState } from 'react'
 
-export default function Profile({ avatar, name,bio, email, phone, 
-                                    githubUrl,linkedinUrl, twitterUrl}) {
+
+export default function Profile({ avatar, name,bio, email, phone, githubUrl,linkedinUrl, twitterUrl}) {
+    const [followText, setFollowText] = useState('Follow')
+    
+    function HandleClick(ev) {
+        alert('Agora você está seguindo')
+        setFollowText("Following")
+    }    
+                                        
     return ( <>
     <div className={styles.container}>
         <img src={avatar} alt={name} className={styles.perfilImg} />
         <Title>
             {name}
-            <button>follow</button> 
+            <button
+            className={styles.followButton}
+                onClick={HandleClick}        
+            >{followText}</button> 
         </Title>
         <ProfileSection>{bio}</ProfileSection>
         <ProfileSection>{ phone }</ProfileSection> 
         <ProfileSection>{ email }</ProfileSection> 
 
-        <ProfileSection>
-            <div className={styles.containerBtn}>
+        <ProfileSection 
+            className={styles.containerBtn}
+            id="links-section"
+            data-test="some value"
+            arial-label = 'social links'
+        >
                 <LinkButton href={githubUrl}>Github 
                 </LinkButton>
                 <LinkButton href={ linkedinUrl }>Linkedin </LinkButton>
                 <LinkButton href={ twitterUrl }>Twitter</LinkButton>
-            </div>
+
         </ProfileSection>
     </div>
 
